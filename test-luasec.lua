aa82413caca41d0e8b021733570059197cba0e7d
Tobiasz Laskowski <tobil4sk@outlook.com>
2026-02-15 17:48:16 +0000

    Test

---
+++ test-luasec.lua
@@ -0,0 +1,14 @@
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+local result = {}
+
+local res, status, headers, err = http.request {
+	url = "https://luarocks.org/manifest-5.1.json",
+	proxy = nil,
+	method = "GET",
+	redirect = false,
+	sink = ltn12.sink.table(result),
+	headers = {
+		["user-agent"] = "LuaRocks/3.13.0 linux-aarch64 via LuaSocket"
+	},
+}
